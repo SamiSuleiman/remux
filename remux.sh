7007e28a3baab4b82f8619526ca4e5dbd4ee984b
Sami Suleiman <sulsami@protonmail.com>
2024-08-22 18:40:21 +0200

    save tmux state to file

--- remux.sh
+++ remux.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+remux_state_file=~/.local/share/remux.txt
+
 function tmux_session_exists(){
   local session_name=$1
   return $(tmux has-session -t ${session_name} &> /dev/null && echo 0 || echo 1)
@@ -9,9 +11,15 @@ function tmux_session_exists(){
 
 function save() {
   local tmux_state=$(tmux lsp -aF '#{session_name}>#{window_index}>#{window_layout}>#{pane_index}>#{pane_current_command}>#{pane_current_path}')
-  local remux_state='{}'
 
-  # Use a while loop to process each line
+  mkdir -p $(dirname "${remux_state_file}")
+
+  printf "${tmux_state}" > "${remux_state_file}"
+}
+
+function restore() {
+  local remux_state=''
+
   while IFS= read -r line; do
     local session_name=$(echo "$line" | cut -d'>' -f1)
     local window_index=$(echo "$line" | cut -d'>' -f2)
@@ -28,12 +36,7 @@ function save() {
 
     printf 'Session: %s, Window: %s, Layout: %s, Pane: %s, Command: %s, Path: %s\n' "${session_name}" "${window_index}" "${window_layout}" "${pane_index}" "${pane_command}" "${pane_path}"
 
-
-  done <<< "${tmux_state}"
-}
-
-function restore() {
-  echo "message"
+  done <<< "${remux_state}"
 }
 
 while getopts ":sr" opt; do
