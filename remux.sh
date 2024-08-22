38e8262f9aceaa9c8a075c6b97b44c0aad6fa724
Sami Suleiman <sulsami@protonmail.com>
2024-08-22 10:16:26 +0200

    wip: save function

--- remux.sh
+++ remux.sh
@@ -3,7 +3,23 @@
 set -euo pipefail
 
 function save() {
-  printf 'Saving tmux state...'
+  local tmux_state=$(tmux lsp -aF '#{session_name}>#{window_index}>#{window_layout}>#{pane_title}>#{pane_current_command}')
+  # the object state, each key is a session name,
+  # each session has a list of windows, each window has a layout and an index and a list of panes
+  # each pane has a command
+  local remux_state='{}'
+
+  # Use a while loop to process each line
+  while IFS= read -r line; do
+    local session_name=$(echo "$line" | cut -d'>' -f1)
+    local window_index=$(echo "$line" | cut -d'>' -f2)
+    local window_layout=$(echo "$line" | cut -d'>' -f3)
+    local pane_title=$(echo "$line" | cut -d'>' -f4)
+    local pane_command=$(echo "$line" | cut -d'>' -f5)
+  
+    printf 'Session: %s, Window: %s, Layout: %s, Pane: %s, Command: %s\n' "$session_name" "$window_index" "$window_layout" "$pane_title" "$pane_command"
+
+  done <<< "$tmux_state"
 }
 
 function restore() {
