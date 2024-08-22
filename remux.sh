56e7ae2caa57aee6157c580e487c7d2256a047eb
Sami Suleiman <sulsami@protonmail.com>
2024-08-22 20:26:40 +0200

    progress

--- remux.sh
+++ remux.sh
@@ -9,8 +9,31 @@ function tmux_session_exists() {
   return $(tmux has-session -t ${session_name} &> /dev/null && echo 0 || echo 1)
 }
 
+function tmux_window_exists() {
+  local session_name="$1"
+  local window_index="$2"
+
+  if tmux list-windows -t "${session_name}" | grep -q "^${window_index}:"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function tmux_pane_exists() {
+  local session_name="$1"
+  local window_index="$2"
+  local pane_index="$3"
+
+  if tmux list-panes -t "${session_name}:${window_index}" | grep -q "^${pane_index}:"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 function save() {
-  local tmux_state=$(tmux lsp -aF '#{session_name}>#{window_index}>#{window_layout}>#{pane_index}>#{pane_current_command}>#{pane_current_path}')
+  local tmux_state=$(tmux lsp -aF '#{session_name}>#{window_index}>#{window_name}>#{window_layout}>#{pane_index}>#{pane_current_command}>#{pane_current_path}')
 
   mkdir -p $(dirname "${remux_state_file}")
 
@@ -18,6 +41,11 @@ function save() {
 }
 
 function restore() {
+  if [ ! -f "${remux_state_file}" ]; then
+    echo "No remux save found"
+    exit 1
+  fi
+
   local remux_state=$(<"${remux_state_file}")
 
   if [ -z "${remux_state}" ]; then
@@ -28,16 +56,23 @@ function restore() {
   while IFS= read -r line; do
     local session_name=$(echo "$line" | cut -d'>' -f1)
     local window_index=$(echo "$line" | cut -d'>' -f2)
-    local window_layout=$(echo "$line" | cut -d'>' -f3)
-    local pane_index=$(echo "$line" | cut -d'>' -f4)
-    local pane_command=$(echo "$line" | cut -d'>' -f5)
-    local pane_path=$(echo "$line" | cut -d'>' -f6)
+    local window_name=$(echo "$line" | cut -d'>' -f3)
+    local window_layout=$(echo "$line" | cut -d'>' -f4)
+    local pane_index=$(echo "$line" | cut -d'>' -f5)
+    local pane_command=$(echo "$line" | cut -d'>' -f6)
+    local pane_path=$(echo "$line" | cut -d'>' -f7)
 
     if ! tmux_session_exists ${session_name}; then
       tmux new-session -d -s ${session_name}
     fi 
 
-    printf 'Session: %s, Window: %s, Layout: %s, Pane: %s, Command: %s, Path: %s\n' "${session_name}" "${window_index}" "${window_layout}" "${pane_index}" "${pane_command}" "${pane_path}"
+    if ! tmux_window_exists ${session_name} ${window_index}; then
+      tmux new-window -d -t ${session_name}:${window_index} -n ${window_name}
+    fi
+
+    if ! tmux_pane_exists ${session_name} ${window_index} ${pane_index}; then
+      tmux split-window -d -t ${session_name}:${window_index} -c ${pane_path}
+    fi
 
   done <<< "${remux_state}"
 }
@@ -55,4 +90,3 @@ while getopts ":sr" opt; do
       ;;
   esac
 done
-
